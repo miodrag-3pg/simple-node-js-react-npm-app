627c4b353e3a8d674ee26436c0e923a607e69a2f
miodrag-3pg <miodrag.petrovici@tim-mb-111.local>
2018-04-04 12:19:32 +0300

    Eslint fix

--- jenkins/scripts/lint.sh
+++ jenkins/scripts/lint.sh
@@ -1,4 +1,4 @@
 echo 'LINT STEP'
 
 npm install eslint -g
-eslint -c config.eslintrc -f checkstyle src/**/*.js > eslint.xml || echo 'LINT FAILED'
+eslint -c config.eslintrc -f checkstyle src/**/*.js || echo 'LINT FAILED'
