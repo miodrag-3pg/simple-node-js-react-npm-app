5d512ee1175e8749a65a4a5c7a8ebfbe90971d39
miodrag-3pg <miodrag.petrovici@tim-mb-111.3pillar.corp>
2018-04-04 10:56:52 +0300

    Comment the lint step

--- jenkins/scripts/lint.sh
+++ jenkins/scripts/lint.sh
@@ -1,4 +1,4 @@
 echo 'LINT STEP'
-npm install eslint
-eslint -c config.eslintrc -f checkstyle src/**/*.js > eslint.xml || echo 'LINT FAILED'
-exit 0
+
+# npm install eslint
+# eslint -c config.eslintrc -f checkstyle src/**/*.js > eslint.xml || echo 'LINT FAILED'
